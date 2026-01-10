d4a1acfe74992b6fdeb8590160d4d45358c93a5e
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-01-10 23:25:06 +0530

    Frontend Update Completed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://todoapp-8i1e.onrender.com"
 });
 
 api.interceptors.request.use((req) => {
